b5273b8e3c098d7c254bd97102cba6487792d65d
Magform <nicolas25.02.2003.nf@gmail.com>
2023-10-28 12:52:17 +0200

    External configuration and more readibility

---
+++ Senk/Configuration.h
@@ -0,0 +1,11 @@
+#ifndef Configuration_H_
+#define Configuration_H_
+
+// We define here all the configuration needed using define:
+#define debug 1 //1 activated, 0 deactivated
+#define distanceData 1000
+
+
+
+
+#endif //Configuration_H_
--- Senk/Senk.ino
+++ Senk/Senk.ino
@@ -4,6 +4,9 @@
 #include <ArduinoBLE.h>
 #include <vector>
 
+#include "Configuration.h"
+
+//Bluetooth Service and characteristics
 BLEService Accelerometer("1101");
 BLEService Gyroscope("1102");
 BLEShortCharacteristic AccX("2101", BLERead);
@@ -13,21 +16,52 @@ BLEShortCharacteristic GyroX("3101", BLERead);
 BLEShortCharacteristic GyroY("3102", BLERead);
 BLEShortCharacteristic GyroZ("3103", BLERead);  
 
-
+//Sensor to read
 SensorXYZ accel(SENSOR_ID_ACC);
 SensorXYZ gyro(SENSOR_ID_GYRO);
 
-#define distanceData 1000
+//util function declaration
+int initializeBLE();
+void sendDataBLE();
 
 void setup() {
-  Serial.begin(115200);
-  
+  if(debug){
+    Serial.begin(115200);
+  }
+
   BHY2.begin();
   accel.begin();
   gyro.begin();
 
-    while(!BLE.begin()) {
-    //BLE problem
+  initializeBLE();
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+  static auto lastCheck = millis();
+  BHY2.update();
+
+  if (millis() - lastCheck >= distanceData){
+    lastCheck = millis();
+    
+    if(debug){
+      Serial.print("Accelerometer: " + accel.toString());
+      Serial.print("Gyroscope: " + gyro.toString());
+      Serial.println();
+    }
+
+    sendDataBLE();
+
+  }
+
+  delay(1);
+}
+
+//Function definition
+int initializeBLE(){
+
+  if(!BLE.begin()) {
+    return 0;
   }
 
   BLE.setLocalName("Senk");
@@ -42,20 +76,11 @@ void setup() {
   BLE.addService(Accelerometer);
   BLE.addService(Gyroscope);
   BLE.advertise();
-
+  
+  return 1;
 }
 
-void loop() {
-  BLEDevice central = BLE.central();
-  static auto lastCheck = millis();
-  BHY2.update();
-
-  if (millis() - lastCheck >= distanceData){
-    lastCheck = millis();
-    Serial.print("Accelerometer: " + accel.toString());
-    Serial.print("Gyroscope: " + gyro.toString());
-    Serial.println();
-
+void sendDataBLE(){
     AccX.writeValue(accel.x());
     AccY.writeValue(accel.y());
     AccZ.writeValue(accel.z());
@@ -63,6 +88,4 @@ void loop() {
     GyroX.writeValue(gyro.x());
     GyroY.writeValue(gyro.y());
     GyroZ.writeValue(gyro.z());
-  }
-  delay(1);
 }
