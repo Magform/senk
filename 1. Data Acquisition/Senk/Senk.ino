45191fe0a59c6e00fb0f1a4877b589b51d9e3a38
Magform <nicolas25.02.2003.nf@gmail.com>
2023-11-03 19:11:44 +0100

    Data Acquisition testing

---
+++ 1. Data Acquisition/Senk/Senk.ino
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+#include "Nicla_System.h"
+#include "Arduino_BHY2.h"
+
+#include "Configuration.h"
+
+//Sensor to read
+SensorXYZ accel(SENSOR_ID_ACC);
+SensorXYZ gyro(SENSOR_ID_GYRO);
+
+void setup() {
+  if(debug){
+    Serial.begin(115200);
+    Serial.print("Initializing ");
+  }
+  
+  BHY2.begin();
+  accel.begin();
+  gyro.begin();
+
+  if(debug){
+    Serial.println(" done!");
+  }
+}
+
+void loop(){
+  static auto lastSet = millis();
+  BHY2.update();
+  
+  if (millis() - lastSet >= distanceBetweenSet){
+    lastSet = millis();
+    takeDataSet();
+  }
+
+  delay(1);
+}
+
+//Take data from accelerometer and gyroscope and add it to the dataset
+void takeDataSet(){
+  for(int i = 0; i<dataPerSet; i++){
+    BHY2.update();
+    short aX = accel.x();
+    short aY = accel.y();
+    short aZ = accel.z();
+    short gX = gyro.x(); 
+    short gY = gyro.y();
+    short gZ = gyro.z();
+    if(debug){
+      Serial.print("Accelerometer (X, Y, Z): (");
+      Serial.print(aX);
+      Serial.print(", ");
+      Serial.print(aY);
+      Serial.print(", ");
+      Serial.print(aZ);
+      Serial.print("); Gyroscope (X, Y, Z): (");
+      Serial.print(gX);
+      Serial.print(", ");
+      Serial.print(gY);
+      Serial.print(", ");
+      Serial.print(gZ);
+      Serial.println(")");
+    }
+    delay(distanceData);
+  }
+}
