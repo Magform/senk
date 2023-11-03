f5ffd503c8381e1b7af36c846ec51d3a507b8a60
Magform <nicolas25.02.2003.nf@gmail.com>
2023-11-03 19:12:09 +0100

    Data Acquisition and storing to RAM

---
+++ 2. Data Acquisition and Storing/1.Senk/1.Senk.ino
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include "Nicla_System.h"
+#include "Arduino_BHY2.h"
+#include <ArduinoBLE.h>
+
+#include "Data.h"
+#include "Configuration.h"
+
+//Sensor to read
+SensorXYZ accel(SENSOR_ID_ACC);
+SensorXYZ gyro(SENSOR_ID_GYRO);
+
+//util function declaration
+void takeDataSet();
+void printDataSet();
+
+Data dataSet[dataPerSet];
+
+void setup() {
+  if(debug){
+    Serial.begin(115200);
+    Serial.print("Initializing ");
+  }
+  
+  BHY2.begin();
+  accel.begin();
+  gyro.begin();
+
+  if(debug){
+    Serial.println(" done!");
+  }
+}
+
+void loop(){
+  static auto lastSet = millis();
+  BHY2.update();
+  
+  if (millis() - lastSet >= distanceBetweenSet){
+    lastSet = millis();
+    takeDataSet();
+    printDataSet();
+  }
+
+  delay(1);
+}
+
+//Function definition
+
+//Take data from accelerometer and gyroscope and add it to the dataset
+void takeDataSet(){
+  for(int i = 0; i<dataPerSet; i++){
+    BHY2.update();
+    dataSet[i] = Data(accel.x(), accel.y(), accel.z(), gyro.x(), gyro.y(), gyro.z());
+    delay(distanceData);
+  }
+}
+
+void printDataSet(){
+  for(int i = 0; i<dataPerSet; i++){
+    if(debug){
+      const char* toPrint = dataSet[i].toString();
+      Serial.println(toPrint);
+      delete[] toPrint;
+    }
+  }
+}
---
+++ 2. Data Acquisition and Storing/1.Senk/Configuration.h
@@ -0,0 +1,10 @@
+#ifndef Configuration_H_
+#define Configuration_H_
+
+// We define here all the configuration needed using define:
+#define debug 1 //if defined as 1 is activated, if defined as 0 is of
+#define distanceBetweenSet 10000//milliseconds 
+#define dataPerSet 10 //50 works
+#define distanceData 10 //milliseconds
+
+#endif //Configuration_H_
