6275e9f2a1511926dbe45be7abe0224f1e6d0089
Magform <nicolas25.02.2003.nf@gmail.com>
2023-11-10 14:34:39 +0100

    Added Data Trasmission with endless DataSet

---
+++ 3. Data Trasmission via BLE/3.Senk/3.Senk.ino
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include "Nicla_System.h"
+#include "Arduino_BHY2.h"
+
+#include "Configuration.h"
+#include "BLEConnection.h"
+#include "Data.h"
+
+//Sensor to read
+SensorXYZ accel(SENSOR_ID_ACC);
+SensorXYZ gyro(SENSOR_ID_GYRO);
+
+void takeDataSetAndSend();
+
+BLEConnection BLECon;
+Data dataSet[std::min(200, dataPerSet)];
+
+void setup() {
+  Serial.begin(115200);
+  if(debugStatus){
+    Serial.print("Initializing ");
+  }
+
+  BHY2.begin();
+  accel.begin();
+  gyro.begin();
+
+  BLECon.initialize();
+
+  if(debugStatus){
+    Serial.println(" done!");
+  }
+}
+
+void loop(){
+  static auto lastSet = millis();
+  BHY2.update();
+  
+  if (millis() - lastSet >= distanceBetweenSet){
+    lastSet = millis();
+    for(int i=0; i<(1+dataPerSet%200); i++){
+      takeDataSet();
+      BLECon.send(dataSet, std::min(200, dataPerSet));
+    }
+  }
+  delay(1);
+}
+
+//Function definition
+
+//Take data from accelerometer and gyroscope and add it to the dataset
+void takeDataSet(){
+  for(int i = 0; i<std::min(200, dataPerSet); i++){
+    BHY2.update();
+    dataSet[i] = Data(accel.x(), accel.y(), accel.z(), gyro.x(), gyro.y(), gyro.z());
+    if(debugStatus){
+      const char* toPrint = dataSet[i].toString();
+      Serial.println(toPrint);
+      delete[] toPrint;
+    }
+    delay(distanceData);
+  }
+}
---
+++ 3. Data Trasmission via BLE/3.Senk/Configuration.h
@@ -0,0 +1,10 @@
+#ifndef Configuration_H_
+#define Configuration_H_
+
+// We define here all the configuration needed using define:
+#define debugStatus 1 //if defined as 1 is activated, if defined as 0 is of
+#define distanceBetweenSet 1//milliseconds 
+#define dataPerSet 1000
+#define distanceData 1 //milliseconds
+
+#endif //Configuration_H_
