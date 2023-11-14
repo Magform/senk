67ef435e88cab228b5a459ab3f2f0f0f1d579417
Magform <nicolas25.02.2003.nf@gmail.com>
2023-11-14 17:40:11 +0100

    Improved based on final Senk

--- 1. Data Acquisition/Senk/Configuration.h
+++ 1. Data Acquisition/Senk/Configuration.h
@@ -1,10 +1,22 @@
 #ifndef Configuration_H_
 #define Configuration_H_
 
-// We define here all the configuration needed using define:
-#define debug 1 //if defined as 1 is activated, if defined as 0 is of
-#define distanceBetweenSet 10000//milliseconds 
-#define dataPerSet 1
-#define distanceData 10 //milliseconds
+// Debugging settings
+#define DEBUG_STATUS 1 // Set to 1 to activate debugging, 0 to deactivate
+
+
+
+// Data configuration
+#define DATA_PER_SET 10 // Total numer of data in a dataset
+#define DATA_DISTANCE 1 // Milliseconds of distance between two different data
+#define DISTANCE_BETWEEN_SET 1 // Milliseconds to wait between two different dataset, The time is measured from the first data of the current dataset to the last data of the previous dataset
+
+
+// Debug printing function
+inline void debugPrint(const char* toPrint){
+  #if DEBUG_STATUS
+    Serial.println(toPrint);
+  #endif
+}
 
 #endif //Configuration_H_
--- 1. Data Acquisition/Senk/Senk.ino
+++ 1. Data Acquisition/Senk/Senk.ino
@@ -8,26 +8,27 @@
 SensorXYZ accel(SENSOR_ID_ACC);
 SensorXYZ gyro(SENSOR_ID_GYRO);
 
+//util func
+
 void setup() {
-  if(debug){
-    Serial.begin(115200);
-    Serial.print("Initializing ");
-  }
+  #ifdef DEBUG_STATUS
+  Serial.begin(115200);
+  #endif
+
+  debugPrint("Initializing ");
   
   BHY2.begin();
   accel.begin();
   gyro.begin();
 
-  if(debug){
-    Serial.println(" done!");
-  }
+  debugPrint(" done!");
 }
 
 void loop(){
-  static auto lastSet = millis();
+  static auto lastSet = millis() - DISTANCE_BETWEEN_SET;
   BHY2.update();
-  
-  if (millis() - lastSet >= distanceBetweenSet){
+
+  if (millis() - lastSet >= DISTANCE_BETWEEN_SET){
     takeDataSet();
     lastSet = millis();
   }
@@ -37,7 +38,7 @@ void loop(){
 
 //Take data from accelerometer and gyroscope and add it to the dataset
 void takeDataSet(){
-  for(int i = 0; i<dataPerSet; i++){
+  for(int i = 0; i<DATA_PER_SET; i++){
     BHY2.update();
     short aX = accel.x();
     short aY = accel.y();
@@ -45,21 +46,21 @@ void takeDataSet(){
     short gX = gyro.x(); 
     short gY = gyro.y();
     short gZ = gyro.z();
-    if(debug){
-      Serial.print("Accelerometer (X, Y, Z): (");
-      Serial.print(aX);
-      Serial.print(", ");
-      Serial.print(aY);
-      Serial.print(", ");
-      Serial.print(aZ);
-      Serial.print("); Gyroscope (X, Y, Z): (");
-      Serial.print(gX);
-      Serial.print(", ");
-      Serial.print(gY);
-      Serial.print(", ");
-      Serial.print(gZ);
-      Serial.println(")");
-    }
-    delay(distanceData);
+    #if DEBUG_STATUS
+    Serial.print("Accelerometer (X, Y, Z): (");
+    Serial.print(aX);
+    Serial.print(", ");
+    Serial.print(aY);
+    Serial.print(", ");
+    Serial.print(aZ);
+    Serial.print("); Gyroscope (X, Y, Z): (");
+    Serial.print(gX);
+    Serial.print(", ");
+    Serial.print(gY);
+    Serial.print(", ");
+    Serial.print(gZ);
+    Serial.print(")\n");
+    #endif
+    delay(DATA_DISTANCE);
   }
 }
