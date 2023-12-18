0d84b2b40912f99975c859747bd4d92d361109d6
Magform <nicolas25.02.2003.nf@gmail.com>
2023-12-18 19:36:38 +0100

    Comment improvement

--- util/conversion/BLE_to_Decimal.cpp
+++ util/conversion/BLE_to_Decimal.cpp
@@ -2,19 +2,18 @@
 #include <string>
 #include <vector>
 
+// Function to convert a hexadecimal string into decimal values
 std::vector<short> convertHexToDecimal(const std::string& hexString) {
     std::vector<short> decimalValues;
 
+    // Convert each 4-character hexadecimal substring into its corresponding decimal value
+    // by reversing the byte order and parsing it from hexadecimal to decimal format.
     for (size_t i = 0; i < hexString.length(); i += 4) {
-        // Extract a 4-character hexadecimal substring
         std::string hexSubstring = hexString.substr(i, 4);
-
-        // Reverse the order of the two bytes (little-endian)
         std::string reversedHexSubstring = hexSubstring.substr(2, 2) + hexSubstring.substr(0, 2);
-
-        // Convert the reversed hexadecimal substring to a decimal value
+        
         short decimalValue = static_cast<short>(std::stoi(reversedHexSubstring, nullptr, 16));
-
+        
         decimalValues.push_back(decimalValue);
     }
 
@@ -26,10 +25,8 @@ int main() {
     std::string bleData;
     std::cin >> bleData;
 
-    // Convert BLE data to decimal values
     std::vector<short> decimalValues = convertHexToDecimal(bleData);
-
-    // Print the decimal values
+    
     std::cout << "Decimal values:";
     for (const auto& value : decimalValues) {
         std::cout << " " << value;
